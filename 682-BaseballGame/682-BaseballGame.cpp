8891f60ce83c9aaf9fef9931e34cb794a62ecbe4
Abhinav Kumar Maurya <abhinavprime835@gmail.com>
2024-04-26 23:46:06 +0530

    Create file

---
+++ 682-BaseballGame/682-BaseballGame.cpp
@@ -0,0 +1,25 @@
+                }
+                }
+                else if(o[i] == "D"){
+                else if(o[i] == "C"){
+                    s.pop();
+                }
+                    s.push(2*s.top());
+                }
+                else{
+                    s.push(stoi(o[i]));
+            }
+        }
+        int sum = 0;
+                    int y = s.top();
+                    s.push(x);
+                    s.push(x+y);
+                if(o[i] == "+"){
+                    int x = s.top();
+                    s.pop();
+        for(int i = 0; i < o.size();i++){
+            if(s.empty()) s.push(stoi(o[i]));
+            else{
+    int calPoints(vector<string>& o) {
+        stack<int> s;
+[
